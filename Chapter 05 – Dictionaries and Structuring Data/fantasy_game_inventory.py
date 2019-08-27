e4b9c618554c5c8ec4831973f3cc710032971d81
MatthieuDasnoy <dasnoy_m@hotmail.com>
2019-08-27 17:18:51 +0200

    Create fantasy_game_inventory.py

---
+++ Chapter 05 – Dictionaries and Structuring Data/fantasy_game_inventory.py
@@ -0,0 +1,22 @@
+player_inventory = {'gold coin': 42, 'rope': 1}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def displayInventory(some_dictionary):
+    item_total = 0
+    print('Inventory:')
+
+    for k, v in some_dictionary.items():
+        print(v, k)
+        item_total += v
+        
+    print('Total number of items: ' + str(item_total))
+
+def addToIventory(some_dictionary, some_list):
+    for i in some_list:
+        some_dictionary.setdefault(i, 0)    #If k key doesn't exist, add it to iventory and set is associated value to 0
+        some_dictionary[i] += 1             #Add 1 to associated value of k key
+
+    return some_dictionary
+
+player_inventory = addToIventory(player_inventory, dragon_loot)
+displayInventory(player_inventory)

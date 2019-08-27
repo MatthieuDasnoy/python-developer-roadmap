4f3a913f265552040c8f1bca3344efd4d3096160
MatthieuDasnoy <dasnoy_m@hotmail.com>
2019-08-27 17:08:17 +0200

    Update comma_code.py

--- Chapter 04 – Lists/comma_code.py
+++ Chapter 04 – Lists/comma_code.py
@@ -2,12 +2,17 @@ spam = ['apples', 'bananas', 'tofu', 'cats']
 
 def listToStr(some_list):
     new_string = ''
-    
-    for i in range(len(some_list)):
-        if i < len(some_list) - 1:
-            new_string += some_list[i] + ', '
-        else:
-            new_string += 'and ' + some_list[i]
+
+    if len(some_list) == 0:
+        new_string = 'The list is empty'
+    elif len(some_list) == 1:
+        new_string = some_list[0]
+    else:
+        for i in range(len(some_list)):
+            if i < len(some_list) - 1:
+                new_string += some_list[i] + ', '
+            else:
+                new_string += 'and ' + some_list[i]
             
     return new_string
 

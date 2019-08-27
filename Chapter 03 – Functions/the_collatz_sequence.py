a3d66aeb283de09e0858b2f033cd8e0b07b02d36
MatthieuDasnoy <dasnoy_m@hotmail.com>
2019-08-27 17:17:12 +0200

    Update the_collatz_sequence.py

--- Chapter 03 – Functions/the_collatz_sequence.py
+++ Chapter 03 – Functions/the_collatz_sequence.py
@@ -1,19 +1,20 @@
 def collatz(number):
     if number % 2 == 0: 
         result = number // 2
+        
     else:               
         result = 3 * number + 1
 
     print(result)
     return(result)
 
-    
 while True:
     try:
         num = int(input("Enter a number : "))
-
+        
         if num <= 1:
             print('Error: The number must be greater than 1.')
+            
         else:
             while num != 1:
                 num = collatz(num)

5f03623d268fdc28a45931d84bafb1488c408a9e
MatthieuDasnoy <dasnoy_m@hotmail.com>
2019-08-27 17:18:07 +0200

    Update comma_code.py

--- Chapter 04 – Lists/comma_code.py
+++ Chapter 04 – Lists/comma_code.py
@@ -4,17 +4,19 @@ def listToStr(some_list):
     new_string = ''
 
     if len(some_list) == 0:
-        new_string = 'The list is empty'
+        new_string = 'The list is empty.'
+        
     elif len(some_list) == 1:
         new_string = some_list[0]
+        
     else:
         for i in range(len(some_list)):
             if i < len(some_list) - 1:
                 new_string += some_list[i] + ', '
+                
             else:
                 new_string += 'and ' + some_list[i]
             
     return new_string
-
 
 print(listToStr(spam))

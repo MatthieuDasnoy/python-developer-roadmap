32acf496dd87ef9c569f10f325f7d0a0607fa7cf
MatthieuDasnoy <dasnoy_m@hotmail.com>
2019-08-28 08:15:13 +0200

    Update strong_password_detection.py

--- Chapter 07 – Pattern Matching with Regular Expressions/strong_password_detection.py
+++ Chapter 07 – Pattern Matching with Regular Expressions/strong_password_detection.py
@@ -6,21 +6,19 @@ def isStrongPassword(some_string):
     regex_3 = re.compile(r'[a-z]')   #At least one lowercase character
     regex_4 = re.compile(r'\d')      #At least one digit
 
-    if regex_1.search(some_string):
-        if regex_2.search(some_string) and regex_3.search(some_string):
-            if regex_4.search(some_string):
-                print('The password is strong.')
-                
-            else:
-                print('The password is not strong. It must contain at least one digit.')
-
-        else:
-            print('The password is not strong. It must contain both uppercase and lowercase characters.')
-
-    else:
+    if not regex_1.search(some_string):
         print('The password is not strong. It must be at least eight characters long.')
 
-isStrongPassword('Azerty12')
+    elif not (regex_2.search(some_string) and regex_3.search(some_string)):
+        print('The password is not strong. It must contain both uppercase and lowercase characters.')
+
+    elif not regex_4.search(some_string):
+        print('The password is not strong. It must contain at least one digit.')
+
+    else:
+        print('The password is strong.')
+    
+isStrongPassword('Azerty123')
 
 
 
